871f265b9a603d1e850cfa52c3763317b9361346
Michael <mveronie@gmail.com>
2017-11-07 05:20:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+
+const notes = require('./notes.js');
+
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'
+}
+
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+}
+
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note', {
+    title: titleOptions
+  })
+  .help()
+  .argv;
+let command = argv._[0];
+
+if (command === 'add') {
+  let note = notes.addNote(argv.title, argv.body);
+  if (note) {
+    console.log('Note created');
+    notes.logNote(note);
+  } else {
+    console.log('Note title taken');
+  }
+} else if (command === 'list') {
+  let allNotes = notes.getAll();
+  console.log(`Pringint ${allNotes.length} note(s)`);
+  allNotes.forEach((note) => {
+    notes.logNote(note);
+  });
+} else if (command === 'read') {
+  let note = notes.getNote(argv.title);
+  if (note) {
+    console.log('Note found');
+    notes.logNote(note);
+  } else {
+    console.log('Note not found');
+  }
+} else if (command === 'remove') {
+  let noteRemoved = notes.removeNote(argv.title);
+  let message = noteRemoved ? 'Note was removed' : 'Note not found';
+  console.log(message);
+} else {
+  console.log('Command not recognized');
+}
---
+++ playground/arrow-function.js
@@ -0,0 +1,16 @@
+let square = (x) => x * x;
+console.log(square(9));
+
+let user = {
+  name: "Mike",
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  }
+};
+
+user.sayHi(1, 2, 3);
